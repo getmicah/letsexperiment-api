59b64dd0d0b638d09474e3d16654b5ef8cce0d7b
Micah Cowell <micahcowell@gmail.com>
2017-08-19 15:48:05 -0700

    cors cors cors

--- dist/routes/Session.js
+++ dist/routes/Session.js
@@ -11,8 +11,9 @@ class SessionRouter {
     }
     middleware() {
         this.router.use((req, res, next) => {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
             next();
         });
     }
